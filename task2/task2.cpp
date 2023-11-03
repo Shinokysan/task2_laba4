e7df7ffe37b7bde4fcaf9cc4f52d3a35dc60d929
Компьютер <@DESKTOP-0TR3ASG>
2023-11-03 15:45:01 +0700

    Добавьте файлы проекта.

---
+++ task2/task2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	/* 11 вариант. 
+	Дано целое число N (> 0).
+	Найти сумму 2 + (N + 1)2 + (N + 2)2 + . . .+ (2·N )2 (целое число).
+	*/
+	setlocale(LC_ALL, "Russian");
+	int n,res,i;
+	res = 0;
+	std::cout << "Введите N";
+	std::cin >> n;
+	for (i = 1; i <= n; i++) {
+		res = res + (n + i) * 2;
+	}
+	res = res + 2;
+	std::cout <<"Результат = "<<res;
+
+}
+
